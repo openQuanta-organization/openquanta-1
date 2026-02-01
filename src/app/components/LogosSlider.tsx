57b673f4b220d80f2764a62494301f769d86961b
Ezekiel544 <chikezieezekiel3@gmail.com>
2026-02-01 21:07:20 +0100

    worked on  the works

--- src/app/components/LogosSlider.tsx
+++ src/app/components/LogosSlider.tsx
@@ -50,7 +50,7 @@ const LogosSlider = () => {
         </div>
 
         <div className="relative w-full overflow-hidden py-4">
-          {/* Gradients for smooth fade edges */}
+          {/* Gradients for smooth fade edgesss */}
           <div className="absolute left-0 top-0 bottom-0 w-20 z-20 bg-gradient-to-r from-black to-transparent pointer-events-none" />
           <div className="absolute right-0 top-0 bottom-0 w-20 z-20 bg-gradient-to-l from-black to-transparent pointer-events-none" />
 
